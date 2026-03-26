f8cf1ec0e777639aebfb86b132e8aa9ef58833ad
Thiago <thiago.oliveira7947@usp.br>
2026-03-26 11:41:27 -0300

    sla, fiz mais alterações

--- shazam.py
+++ shazam.py
@@ -1,2 +1,13 @@
+#/usr/bin/python
+
 def sum(a, b):
 	return a + b
+
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
